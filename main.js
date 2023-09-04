7480995baad30207989b9e303d912eecaf7b79ec
shubomifashakin <subomifasakin@outlook.com>
2023-09-04 10:02:26 +0100

    loader corrections

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ import { ThirdSection } from "./js/_thirdSection";
 import { Section4 } from "./js/_fourthSection";
 
 window.addEventListener("load", function () {
+  //allow scrolling
+  this.document.querySelector("body").style.overflowY = "auto";
+  //hide the pre-loader
   document.querySelector(".pre-loader").style.display = "none";
 
   //animate landing section into view
